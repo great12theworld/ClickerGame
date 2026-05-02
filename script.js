6655e402ee9f54ee653c7a6c85a75216feea4e39
great12theworld <88846533+great12theworld@users.noreply.github.com>
2026-05-02 19:50:32 -0400

    added exponential growth to the clicker thing

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
-let clicks = 0;
+let clicks = 0; //total click count right now
 
+
+let clickLevel = 1; //indicative of amount of clicks per second
+
+let clickerUpgrade = 0; //indicative of base upgrade count
+let clickerUpgrade1Price = 10; //indicative of price for next upgrade, grows per upgrade
+
+
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+// basic counting for base multiplicative clicker
 function ClickCounter() {
-    clicks += 1;
+    clicks += clickLevel;
     document.getElementById("clicks").innerHTML = clicks;
 }
 
+function exponentialPriceGrowth(itemgrowingprice, exponent) {
+    return Math.floor(Math.pow((itemgrowingprice * 1.5), exponent));
+}
 
-function ChangeClicker() {
+function addClicker() {
+    if (clicks >= clickerUpgrade1Price) {
+        clicks -= clickerUpgrade1Price;
+        clickerUpgrade += 1;
+        document.getElementById("clickerUpgrade").innerHTML = clickerUpgrade;
+        clickerUpgrade1Price = exponentialPriceGrowth(clickerUpgrade1Price, 1.15); // Increase price for next upgrade
+        updateClickCountUpgrade1(); // runs the clicker made in the background
+    }
+}
+
+async function updateClickCountUpgrade1() {
+    await sleep(5000);
+    clicks += 1
+}
+
+
+async function ChangeClicker() {
     var x = document.getElementById("myDIV");
     if (x.innerHTML === "Hello") {
         x.innerHTML = "Swapped text!";
